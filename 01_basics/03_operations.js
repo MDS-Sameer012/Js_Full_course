f6889b4b34e66e631133b1b8fd08c847ec7220c1
Mohammad Sameer <155916546+MDS-Sameer012@users.noreply.github.com>
2024-01-11 19:13:19 +0000

    03_operations file

---
+++ 01_basics/03_operations.js
@@ -0,0 +1,47 @@
+let positive = 3 ;
+let negative = -positive;
+// console.log("Positive Value : "+positive);
+// console.log("negative Value : "+negative);
+
+// console.log("2+2 : ", 2+2);
+// console.log("2-2 : ", 2-2);
+// console.log("2*2 : ", 2*2);
+// console.log("2**3 : ", 2**3);
+// console.log("2/2 : ", 2/2);
+// console.log("5%2 : ", 5%2);
+// Result : 
+// 2+2 :  4
+// 2-2 :  0
+// 2*2 :  4
+// 2**3 :  8
+// 2/2 :  1
+// 5%2 :  1
+
+// console.log(true+true); // 2
+// console.log(+true+true-true); //2
+// console.log(+true); // it will type cast and return 1
+
+// console.log(typeof(+true)); // number
+
+let first_name = "Mohammad";
+let last_name = "Sameer";
+let full_name = first_name+" "+last_name;
+// console.log(full_name);
+
+// console.log("2"+2); //22
+// console.log(typeof("2"+2)); //String
+// console.log(2+"2"); //22
+// console.log(typeof(2+"2")); //String
+
+// console.log("2"+2+2); //222
+// console.log(typeof("2"+2+2)); //String
+// console.log(3+2+"2"); //52
+// console.log(typeof(3+2+"2")); //String
+
+// Pre & Post Increment/Decrement Operations
+let counter = 22;
+// console.log("Pre Increment : ",++counter); //23 --> it will first increment value and then use
+// console.log("Post Increment : ",counter++); //23 --> it will first use value and then increment
+// console.log("Post Increment : ",counter); //24 --> result of post increment
+
+
