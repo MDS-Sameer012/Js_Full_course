0a84ace7a964b830c1f6bac8c380c75ebcabf865
Mohammad Sameer <155916546+MDS-Sameer012@users.noreply.github.com>
2024-01-30 17:49:00 +0000

    switch statement in js

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,49 @@
+// let age = 15;
+// switch (age) {
+// case 18:
+//         console.log(`You are boy , age : ${age}`);
+//         break;
+//         // case (age > 18):  // --->  not 
+//     case 20:
+//        console.log(`you are man , age : ${age}`);
+//        break;
+//        // case (age < 18):  // --->  not valid
+//        case 30:
+//         console.log(`you are responsible man , age : ${age}`);
+//         break;
+//     default:
+//         console.log(`Enter Valid Age `);
+//         console.log(`you enter : ${age}`);
+//         break;
+// }
+
+//********************************************************
+// string in switch statement
+
+let month = "wed";
+switch (month) {
+    case "mon":
+        console.log(`Monday`);
+        break;
+    case "tue":
+        console.log(`Tuesday`);
+        break;
+    case "wed":
+        console.log(`Wednesday`);
+        break;
+    case "thu":
+        console.log(`Thursday`);
+        break;
+    case "fri":
+        console.log(`Friday`);
+        break;
+    case "sat":
+        console.log(`Saturday`);
+        break;
+    case "sun":
+        console.log(`Sunday`);
+        break;
+    default:
+        console.log(`Invalid Entry`);
+        break;
+}
